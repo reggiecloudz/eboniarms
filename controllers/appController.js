d1f7e4c0557137e04130713abbbfc829e0a72f3d
reggiecloudz <reggiechapple@icloud.com>
2022-11-29 04:03:26 -0800

    more testing

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,78 +1,93 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
-const ROLE_LIST = require("../models/role_list");
-const User = require("../models/User");
+const ROLE_LIST = require('../models/role_list');
+const User = require('../models/User');
 
 exports.landing_page = (req, res) => {
-    res.render("landing");
+  res.render('landing');
 };
 
 exports.login_get = (req, res) => {
-    const error = req.session.error;
-    delete req.session.error;
-    res.render("login", { err: error });
+  const error = req.session.error;
+  delete req.session.error;
+  res.render('login', { err: error });
 };
 
 exports.login_post = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (!user) {
-        req.session.error = "Invalid Credentials";
-        return res.redirect("/login");
-    }
+  if (!user) {
+    req.session.error = 'Invalid Credentials';
+    return res.redirect('/login');
+  }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+  const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isMatch) {
-        req.session.error = "Invalid Credentials";
-        return res.redirect("/login");
-    }
+  if (!isMatch) {
+    req.session.error = 'Invalid Credentials';
+    return res.redirect('/login');
+  }
 
-    req.session.isAuth = true;
-    req.session.user = user;
-    res.redirect("/dashboard");
+  req.session.isAuth = true;
+  req.session.user = user;
+  res.redirect('/dashboard');
 };
 
 exports.register_get = (req, res) => {
-    const error = req.session.error;
-    delete req.session.error;
-    res.render("register", { err: error });
+  const error = req.session.error;
+  delete req.session.error;
+  res.render('register', { err: error });
 };
 
 exports.register_post = async (req, res) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    let user = await User.findOne({ email });
+  let user = await User.findOne({ email });
 
-    if (user) {
-        req.session.error = "User already exists";
-        return res.redirect("/register");
-    }
+  if (user) {
+    req.session.error = 'User already exists';
+    return res.redirect('/register');
+  }
 
-    const hasdPsw = await bcrypt.hash(password, 12);
+  const hasdPsw = await bcrypt.hash(password, 12);
 
-    user = new User({
-        username,
-        email,
-        password: hasdPsw,
-        roles: [ROLE_LIST.Admin]
-    });
+  user = new User({
+    username,
+    email,
+    password: hasdPsw,
+    roles: [ROLE_LIST.Admin]
+  });
 
-    await user.save();
-    res.redirect("/login");
+  await user.save();
+  res.redirect('/login');
 };
 
 exports.dashboard_get = (req, res) => {
-    const user = req.session.user;
-    console.log(user);
-    res.render("dashboard", {user: user});
+  const user = req.session.user;
+  // console.log(user);
+  res.render('dashboard', { user: user });
 };
 
 exports.logout_post = (req, res) => {
-    req.session.destroy((err) => {
-        if (err) throw err;
-        res.redirect("/login");
+  req.session.destroy((err) => {
+    if (err) throw err;
+    res.redirect('/login');
+  });
+};
+
+exports.matches_get = (req, res) => {
+  res.render('matches');
+};
+
+exports.new_message_get = (req, res) => {
+  // You can do validation or database stuff before emiting
+  req.io.on('connection', (socket) => {
+    socket.on('new-message', (msg) => {
+      console.log(msg);
+      socket.emit('response', 'Message received');
     });
+  });
+  return res.render('messages');
 };
--- server.js
+++ server.js
@@ -1,54 +1,78 @@
-const express = require("express");
-const session = require("express-session");
-const MongoDBSession = require("connect-mongodb-session")(session);
-const mongoose = require("mongoose");
-const appController = require("./controllers/appController");
-const isAuth = require("./middleware/is-auth");
-const verifyRoles = require("./middleware/verifyRoles");
-const ROLE_LIST = require("./models/role_list");
+const { createServer } = require('http'); // you can use https as well
+const express = require('express');
+const socketIo = require('socket.io');
+const session = require('express-session');
+const MongoDBSession = require('connect-mongodb-session')(session);
+const mongoose = require('mongoose');
+const appController = require('./controllers/appController');
+const isAuth = require('./middleware/is-auth');
+const verifyRoles = require('./middleware/verifyRoles');
+const ROLE_LIST = require('./models/role_list');
 
 const app = express();
 
-const mongoUri = "mongodb://localhost:27017/eboniarms";
+const server = createServer(app);
+const io = socketIo(server, { cors: { origin: '*' } }); // you can change the cors to your own domain
+
+app.use((req, res, next) => {
+  req.io = io;
+  return next();
+});
+
+const mongoUri = 'mongodb://localhost:27017/eboniarms';
 
 mongoose.connect(mongoUri, {}).then((res) => {
-    console.log("MongoDB Connected");
+  console.log('MongoDB Connected');
 });
 
 const store = new MongoDBSession({
-    uri: mongoUri,
-    collection: "sessions",
+  uri: mongoUri,
+  collection: 'sessions'
 });
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.static('node_modules/bootstrap-icons'));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
-    secret: "VGgxJE4wdDRIdW1Abkl6",
+app.use(
+  session({
+    secret: 'VGgxJE4wdDRIdW1Abkl6',
     resave: false,
     saveUninitialized: false,
     store: store
-}));
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.isAuth = req.session.isAuth;
+  next();
+});
 
 //=================== Routes
 // Landing Page
-app.get("/", appController.landing_page);
+app.get('/', appController.landing_page);
+
+// socket.io
+app.get('/new-message', appController.new_message_get);
 
 // Login Page
-app.get("/login", appController.login_get);
-app.post("/login", appController.login_post);
+app.get('/login', appController.login_get);
+app.post('/login', appController.login_post);
 
 // Register Page
-app.get("/register", appController.register_get);
-app.post("/register", appController.register_post);
+app.get('/register', appController.register_get);
+app.post('/register', appController.register_post);
+
+app.get('/matches', appController.matches_get);
 
 // Dashboard Page
-app.get("/dashboard", isAuth, verifyRoles(ROLE_LIST.Admin, ROLE_LIST.Moderator), appController.dashboard_get);
+app.get('/dashboard', isAuth, verifyRoles(ROLE_LIST.Admin, ROLE_LIST.Moderator), appController.dashboard_get);
 
-app.post("/logout", appController.logout_post);
+app.get('/logout', appController.logout_post);
 
-app.get("/access-denied", (req, res) => {
-    res.render("accessDenied");
+app.get('/access-denied', (req, res) => {
+  res.render('accessDenied');
 });
 
-app.listen(3000, console.log("Server running on http://localhost:3000"));
+server.listen(3000, () => console.log(`Server started on port 3000!`));
